e6f227c7df8a989437a43979ac89b685fd085a9b
Axel Esquivel <2073218250101@ingenieria.usac.edu.gt>
2022-03-07 17:32:08 -0600

    creado modelo nodo

---
+++ paquetes/modelos/nodo.py
@@ -0,0 +1,8 @@
+class Nodo:
+    siguiente: None
+    
+    def set_siguiente(self, siguiente):
+        self.siguiente = siguiente
+        
+    def get_siguiente(self):
+        return self.siguiente

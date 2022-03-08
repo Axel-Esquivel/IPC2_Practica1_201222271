0ca54edac297d64abba3f64fe937bf43d1db64b5
Axel Esquivel <2073218250101@ingenieria.usac.edu.gt>
2022-03-08 16:49:52 -0600

    agregada opcion pizzas

--- paquetes/menus/menu_pizzas.py
+++ paquetes/menus/menu_pizzas.py
@@ -1,3 +1,10 @@
+from ..listas.pizzas import Pizzas
+
 class MenuPizzas:
+    __pizzas: Pizzas = None
+    
+    def __init__(self, pizzas: Pizzas):
+        self.__pizzas = pizzas
+    
     def mostrar_menu(self):
         volver = False

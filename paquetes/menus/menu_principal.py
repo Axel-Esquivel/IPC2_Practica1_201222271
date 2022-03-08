ee243db58521c173ab9d49ac29ce375c4a3e41bb
Axel Esquivel <2073218250101@ingenieria.usac.edu.gt>
2022-03-08 15:56:12 -0600

    cambio del nombre de opciones

--- paquetes/menus/menu_principal.py
+++ paquetes/menus/menu_principal.py
@@ -6,11 +6,12 @@ class MenuPrincipal:
         
         while salir == False:
             print('-'*20 + 'MENU PRINCIPAL' + '-'*20)
-            print('{0}. {1}'.format(OpcionesMenuPrincipal.Crear_Ingrediente.value, OpcionesMenuPrincipal.Crear_Ingrediente.name.replace('_', ' ')))
+            print('{0}. {1}'.format(OpcionesMenuPrincipal.Ingredientes.value, OpcionesMenuPrincipal.Ingredientes.name.replace('_', ' ')))
+            print('{0}. {1}'.format(OpcionesMenuPrincipal.Pizzas.value, OpcionesMenuPrincipal.Pizzas.name.replace('_', ' ')))
             print('{0}. {1}'.format(OpcionesMenuPrincipal.Salir.value, OpcionesMenuPrincipal.Salir.name.replace('_', ' ')))
             
             opcion = input()
             
             if opcion.isnumeric():
-                if opcion == OpcionesMenuPrincipal.Salir.value:
-                    Salir = True
+                if int(opcion) == int(OpcionesMenuPrincipal.Salir.value):
+                    salir = True

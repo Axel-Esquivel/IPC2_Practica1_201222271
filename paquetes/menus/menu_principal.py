210588381e5d962d2384eb49cad957ebd8447ab0
Axel Esquivel <2073218250101@ingenieria.usac.edu.gt>
2022-03-08 15:38:47 -0600

    sale del while

--- paquetes/menus/menu_principal.py
+++ paquetes/menus/menu_principal.py
@@ -7,3 +7,10 @@ class MenuPrincipal:
         while salir == False:
             print('-'*20 + 'MENU PRINCIPAL' + '-'*20)
             print('{0}. {1}'.format(OpcionesMenuPrincipal.Crear_Ingrediente.value, OpcionesMenuPrincipal.Crear_Ingrediente.name))
+            print('{0}. {1}'.format(OpcionesMenuPrincipal.Salir.value, OpcionesMenuPrincipal.Salir.name))
+            
+            opcion = input()
+            
+            if opcion.isnumeric():
+                if opcion == OpcionesMenuPrincipal.Salir.value:
+                    Salir = True

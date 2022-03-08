cf78e1162b8d1a442bd3c2a1697d03fbe6358331
Axel Esquivel <2073218250101@ingenieria.usac.edu.gt>
2022-03-08 15:36:09 -0600

    agregado primer opcion

--- paquetes/menus/menu_principal.py
+++ paquetes/menus/menu_principal.py
@@ -1,6 +1,9 @@
+from ..enums.opciones_menu_principal import OpcionesMenuPrincipal
+
 class MenuPrincipal:
     def mostrar_menu(self):
         salir = False
         
         while salir == False:
             print('-'*20 + 'MENU PRINCIPAL' + '-'*20)
+            print('{0}. {1}'.format(OpcionesMenuPrincipal.Crear_Ingrediente.value, OpcionesMenuPrincipal.Crear_Ingrediente.name))

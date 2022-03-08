84b4ea291ac7fed2e8b0002588d16eccdb1da080
Axel Esquivel <2073218250101@ingenieria.usac.edu.gt>
2022-03-08 16:48:38 -0600

    agregada propiedad ordenes

--- paquetes/menus/menu_ordenes.py
+++ paquetes/menus/menu_ordenes.py
@@ -1,3 +1,10 @@
+from ..listas.ordenes import Ordenes
+
 class MenuOrdenes:
+    __ordenes: Ordenes = None
+    
+    def __init__(self, ordenes: Ordenes):
+        self.__ordenes = ordenes
+    
     def mostrar_menu(self):
         volver = False

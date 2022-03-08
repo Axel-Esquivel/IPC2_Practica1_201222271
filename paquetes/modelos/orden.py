a087de5e4415de81c2020070fa50d887f6a799be
Axel Esquivel <2073218250101@ingenieria.usac.edu.gt>
2022-03-08 02:03:48 -0600

    creado modelo orden

--- paquetes/modelos/orden.py
+++ paquetes/modelos/orden.py
@@ -1,6 +1,25 @@
 from .cliente import Cliente
+from ..listas.pizzas import Pizzas
 
 class Orden:
-    numero: int = None
-    cliente: Cliente = None
+    __numero: int = None
+    __pizzas: Pizzas = None
+    __cliente: Cliente = None
     
+    def set_numero(self, numero: int):
+        self.__numero = numero
+        
+    def set_pizzas(self, pizzas: Pizzas):
+        self.__pizzas = pizzas
+        
+    def set_cliente(self, cliente: Cliente):
+        self.__cliente = cliente
+        
+    def get_numero(self):
+        return self.__numero
+    
+    def get_pizzas(self):
+        return self.__pizzas
+    
+    def get_cliente(self):
+        return self.__cliente

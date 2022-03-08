e79d8d64acbd2f0bb0c8f74e9d2033c7f908a323
Axel Esquivel <2073218250101@ingenieria.usac.edu.gt>
2022-03-07 22:24:12 -0600

    agregados metodos get set para prop siguiente

--- paquetes/modelos/nodo_ingrediente.py
+++ paquetes/modelos/nodo_ingrediente.py
@@ -4,8 +4,14 @@ from .ingrediente import Ingrediente
 class NodoIngrediente(Nodo):
     __valor: Ingrediente = None
     
+    def set_siguiente(self, siguiente):
+        self.__siguiente = siguiente
+    
     def set_ingrediente(self, ingrediente: Ingrediente):
         self.__valor = ingrediente
-        
+    
+    def get_siguiente(self):
+        return self.__siguiente
+    
     def get_ingrediente(self):
         return self.__valor

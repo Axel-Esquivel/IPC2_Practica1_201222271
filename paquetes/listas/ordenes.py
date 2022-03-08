1b91495b55a50169607a4785799cda952d711fb4
Axel Esquivel <2073218250101@ingenieria.usac.edu.gt>
2022-03-08 15:02:22 -0600

    creados metodos pila ordenes

--- paquetes/listas/ordenes.py
+++ paquetes/listas/ordenes.py
@@ -1,3 +1,71 @@
+from ..modelos.orden import Orden
+from ..modelos.nodo_orden import NodoOrden
+
 class Ordenes:
+    __conteo: int = None
+    __puntero: NodoOrden = None
+    __temporal: NodoOrden = None
     
-    pass
+    def __init__(self):
+        self.__conteo = 0
+        self.__puntero = NodoOrden()
+        
+    def __iter__(self):
+        self.__temporal = self.__puntero
+        
+        while self.__temporal:
+            if self.__temporal.get_orden() != None: yield self.__temporal.get_orden()
+            self.__temporal = self.__temporal.get_siguiente()
+    
+        return self.__conteo
+        
+    def agregar(self, orden: Orden) -> Orden:
+        self.__temporal = self.__puntero
+        
+        while self.__temporal.get_siguiente() != None:
+            self.__temporal = self.__temporal.get_siguiente()
+        
+        self.__conteo += 1
+        nuevo_nodo = NodoOrden()
+        nuevo_nodo.set_orden(orden)
+        self.__temporal.set_siguiente(nuevo_nodo)
+        return orden
+        
+    def eliminar_indice(self, indice: int) -> Orden:
+        self.__temporal = self.__puntero
+        
+        indice_recorrido = -1
+        while indice_recorrido != indice:
+            indice_recorrido += 1
+            if self.__temporal.get_siguiente() != None:
+                self.__temporal = self.__temporal.get_siguiente()
+        
+        if indice_recorrido == indice and self.__temporal.get_siguiente() != None:
+            self.__conteo -= 1
+            return self.__temporal.set_siguiente(self.__temporal.get_siguiente().get_siguiente())
+    
+    def eliminar_elemento(self, orden: Orden) -> Orden:
+        self.__temporal = self.__puntero
+        
+        while self.__temporal.get_siguiente() != None:
+            if self.__temporal.get_siguiente() != None and self.__temporal.get_siguiente().get_orden() == orden:
+                self.__conteo -= 1
+                self.__temporal.set_siguiente(self.__temporal.get_siguiente().get_siguiente())
+            
+            self.__temporal = self.__temporal.get_siguiente()
+            
+        return orden
+    
+    def elemento(self, indice: int) -> Orden:
+        self.__temporal = self.__puntero
+        
+        indice_recorrido = -1
+        while indice_recorrido != indice:
+            indice_recorrido += 1
+            self.__temporal = self.__temporal.get_siguiente()
+        
+        if indice_recorrido == indice and self.__temporal != None:
+            return self.__temporal.get_orden()
+        
+    def contar(self):
+        return self.__conteo

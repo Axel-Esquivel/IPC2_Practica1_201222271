1a71e32db320b2557deb926d5142738ed5a8964c
Axel Esquivel <2073218250101@ingenieria.usac.edu.gt>
2022-03-08 00:12:09 -0600

    creado modelo cliente

---
+++ paquetes/modelos/cliente.py
@@ -0,0 +1,22 @@
+class Cliente:
+    __nombre: str = None
+    __direccion: str = None
+    __NIT: str = None
+    
+    def set_nombre(self, nombre: str):
+        self.__nombre = nombre
+        
+    def set_direccion(self, direccion: str):
+        self.__direccion = direccion
+        
+    def set_NIT(self, NIT: str):
+        self.__NIT = NIT
+        
+    def get_nombre(self) -> str:
+        return self.__nombre
+    
+    def get_direccion(self) -> str:
+        return self.__direccion
+    
+    def get_NIT(self) -> str:
+        return self.__NIT

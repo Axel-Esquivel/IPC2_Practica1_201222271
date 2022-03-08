409d54f281cc75149b006e8c5cda654db615c3dd
Axel Esquivel <2073218250101@ingenieria.usac.edu.gt>
2022-03-08 17:50:03 -0600

    modificado constructor

--- paquetes/modelos/ingrediente.py
+++ paquetes/modelos/ingrediente.py
@@ -2,9 +2,9 @@ class Ingrediente:
     __nombre: str = None
     __tiempo_preparacion: int = None
     
-    def __init__(self, nombre: str, tiempo: int):
+    def __init__(self, nombre: str = None, tiempo_preparacion: int = None):
         self.set_nombre(nombre)
-        self.set_tiempo(tiempo)
+        self.set_tiempo(tiempo_preparacion)
     
     def set_nombre(self, nombre: str):
         self.__nombre = nombre

90eaffc04924171ed32f229f4ac3ec04fa84eaad
Axel Esquivel <2073218250101@ingenieria.usac.edu.gt>
2022-03-08 16:47:22 -0600

    agregada propiedad ingredidetes

--- paquetes/menus/menu_ingredientes.py
+++ paquetes/menus/menu_ingredientes.py
@@ -1,3 +1,10 @@
+from ..listas.ingredientes import Ingredientes
+
 class MenuIngredientes:
+    __ingredientes: Ingredientes = None
+    
+    def __init__(self, ingredientes: Ingredientes):
+        self.__ingredientes = ingredientes
+    
     def mostrar_menu(self):
         volver = False

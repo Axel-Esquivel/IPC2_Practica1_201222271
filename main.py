629e9b121496011852a4b454a38d6e5dca74d84e
Axel Esquivel <2073218250101@ingenieria.usac.edu.gt>
2022-03-07 21:56:29 -0600

    pruebas en main

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+from paquetes.modelos.ingrediente import Ingrediente
+from paquetes.listas.ingredientes import Ingredientes
+
 def main():
-    print('main')
-    pass
+    ingredientes = Ingredientes()
+    ingredientes.agregar(Ingrediente('Pepperoni', 3))
+    ingredientes.agregar(Ingrediente('Salchicha', 4))
+    ingredientes.agregar(Ingrediente('Carne', 10))
+    ingredientes.agregar(Ingrediente('Queso', 5))
+    ingredientes.agregar(Ingrediente('Piña', 2))
+    
+    for ingrediente in ingredientes:
+        print(ingrediente.get_nombre(), ingrediente.get_tiempo())
+    
 
 if __name__ == '__main__':
     main()

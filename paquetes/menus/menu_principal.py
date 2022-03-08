acdedf92ff32daaa1ff078067e38aa72343cc815
Axel Esquivel <2073218250101@ingenieria.usac.edu.gt>
2022-03-08 15:58:44 -0600

    agregada opcion ordenes

--- paquetes/menus/menu_principal.py
+++ paquetes/menus/menu_principal.py
@@ -8,6 +8,7 @@ class MenuPrincipal:
             print('-'*20 + 'MENU PRINCIPAL' + '-'*20)
             print('{0}. {1}'.format(OpcionesMenuPrincipal.Ingredientes.value, OpcionesMenuPrincipal.Ingredientes.name.replace('_', ' ')))
             print('{0}. {1}'.format(OpcionesMenuPrincipal.Pizzas.value, OpcionesMenuPrincipal.Pizzas.name.replace('_', ' ')))
+            print('{0}. {1}'.format(OpcionesMenuPrincipal.Ordenes.value, OpcionesMenuPrincipal.Ordenes.name.replace('_', ' ')))
             print('{0}. {1}'.format(OpcionesMenuPrincipal.Salir.value, OpcionesMenuPrincipal.Salir.name.replace('_', ' ')))
             
             opcion = input()

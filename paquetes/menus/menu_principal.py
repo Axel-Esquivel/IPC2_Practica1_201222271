5cce0527d06020e7860df70032fd99c455cdc469
Axel Esquivel <2073218250101@ingenieria.usac.edu.gt>
2022-03-08 16:42:59 -0600

    mostrar desarrollador

--- paquetes/menus/menu_principal.py
+++ paquetes/menus/menu_principal.py
@@ -2,6 +2,7 @@ from ..listas.pizzas import Pizzas
 from .menu_pizzas import MenuPizzas
 from ..listas.ordenes import Ordenes
 from .menu_ordenes import MenuOrdenes
+from ..componentes.consola import Consola
 from ..listas.ingredientes import Ingredientes
 from .menu_ingredientes import MenuIngredientes
 from ..enums.opciones_menu_principal import OpcionesMenuPrincipal
@@ -18,8 +19,15 @@ class MenuPrincipal:
     
     def mostrar_menu(self):
         salir = False
+        mostar_desarrollador = False
+        Consola.limpiar_consola()
         
         while salir == False:
+            if mostar_desarrollador == True:
+                mostar_desarrollador = False
+                print('-'*20 + 'DESARROLLADOR' + '-'*20)
+                print('201222271 Axel Josué Esquivel Cuy')
+            
             print('-'*20 + 'MENU PRINCIPAL' + '-'*20)
             print('{0}. {1}'.format(OpcionesMenuPrincipal.Ingredientes.value, OpcionesMenuPrincipal.Ingredientes.name.replace('_', ' ')))
             print('{0}. {1}'.format(OpcionesMenuPrincipal.Pizzas.value, OpcionesMenuPrincipal.Pizzas.name.replace('_', ' ')))
@@ -31,9 +39,11 @@ class MenuPrincipal:
             if opcion.isnumeric():
                 if int(opcion) == int(OpcionesMenuPrincipal.Ingredientes.value):
                     MenuIngredientes(self.__ingredientes).mostrar_menu()
-                if int(opcion) == int(OpcionesMenuPrincipal.Pizzas.value):
+                elif int(opcion) == int(OpcionesMenuPrincipal.Pizzas.value):
                     MenuPizzas(self.__pizzas).mostrar_menu()
-                if int(opcion) == int(OpcionesMenuPrincipal.Ordenes.value):
+                elif int(opcion) == int(OpcionesMenuPrincipal.Ordenes.value):
                     MenuOrdenes(self.__ordenes).mostrar_menu()
-                if int(opcion) == int(OpcionesMenuPrincipal.Salir.value):
+                elif int(opcion) == int(OpcionesMenuPrincipal.Desarrollador.value):
+                    mostar_desarrollador = True
+                elif int(opcion) == int(OpcionesMenuPrincipal.Salir.value):
                     salir = True

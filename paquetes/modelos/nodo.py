310cdfa7d00cca90ccd490128e1dfddbd569782a
Axel Esquivel <2073218250101@ingenieria.usac.edu.gt>
2022-03-07 18:05:22 -0600

    agregada propiedad valor y sus metodos

--- paquetes/modelos/nodo.py
+++ paquetes/modelos/nodo.py
@@ -1,8 +1,15 @@
 class Nodo:
+    __valor: None
     __siguiente: None
+    
+    def set_valor(self, valor):
+        self.__valor = valor
     
     def set_siguiente(self, siguiente):
         self.__siguiente = siguiente
+        
+    def get_valor(self):
+        return self.__valor
         
     def get_siguiente(self):
         return self.__siguiente

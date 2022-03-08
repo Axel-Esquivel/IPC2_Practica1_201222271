61ee6cc3ef2cb3d6d5af5bf29489246b91a09c2c
Axel Esquivel <2073218250101@ingenieria.usac.edu.gt>
2022-03-07 22:46:08 -0600

    eliminacion de metodos duplicados con padre

--- paquetes/modelos/nodo_ingrediente.py
+++ paquetes/modelos/nodo_ingrediente.py
@@ -6,15 +6,9 @@ class NodoIngrediente(Nodo):
     
     def __init__(self):
         super()
-    
-    def set_siguiente(self, siguiente):
-        super().set_siguiente(siguiente)
-    
+        
     def set_ingrediente(self, ingrediente: Ingrediente):
         self.__valor = ingrediente
-    
-    def get_siguiente(self):
-        return super().get_siguiente()
     
     def get_ingrediente(self):
         return self.__valor

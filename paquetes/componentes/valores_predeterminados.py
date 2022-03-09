d53c454b9a5b3b93e93faeeef8dbd1851f91c5c8
Axel Esquivel <2073218250101@ingenieria.usac.edu.gt>
2022-03-08 19:36:54 -0600

    agregados valores predeterminados para pizzas

--- paquetes/componentes/valores_predeterminados.py
+++ paquetes/componentes/valores_predeterminados.py
@@ -11,6 +11,7 @@ class ValoresPredeterminados:
         self.__pizzas = pizzas
         self.__ingredientes = ingredientes
         self.__crear_ingredientes()
+        self.__crear_pizzas()
         
     def __crear_ingredientes(self):
         self.__ingredientes.agregar(Ingrediente('Pepperoni', 3))
@@ -18,3 +19,20 @@ class ValoresPredeterminados:
         self.__ingredientes.agregar(Ingrediente('Carne', 10))
         self.__ingredientes.agregar(Ingrediente('Queso', 5))
         self.__ingredientes.agregar(Ingrediente('Piña', 2))
+        
+    def __crear_pizzas(self):
+        ingredientes_pepperoni = Ingredientes()
+        ingredientes_pepperoni.agregar(self.__ingredientes.elemento(0))
+        ingredientes_pepperoni.agregar(self.__ingredientes.elemento(3))
+        self.__pizzas.agregar(Pizza('Pepperoni', 45.00, 1, ingredientes_pepperoni))
+        
+        ingredientes_carnes = Ingredientes()
+        ingredientes_carnes.agregar(self.__ingredientes.elemento(1))
+        ingredientes_carnes.agregar(self.__ingredientes.elemento(2))
+        ingredientes_carnes.agregar(self.__ingredientes.elemento(3))
+        self.__pizzas.agregar(Pizza('Carnes', 90.00, 1, ingredientes_carnes))
+        
+        ingredientes_piña = Ingredientes()
+        ingredientes_piña.agregar(self.__ingredientes.elemento(3))
+        ingredientes_piña.agregar(self.__ingredientes.elemento(4))
+        self.__pizzas.agregar(Pizza('Pepperoni', 45.00, 1, ingredientes_piña))

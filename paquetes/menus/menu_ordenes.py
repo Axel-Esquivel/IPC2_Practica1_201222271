261a9989f8d1725e513dda8156604417c3d41a01
Axel Esquivel <2073218250101@ingenieria.usac.edu.gt>
2022-03-09 11:05:25 -0600

    creado metodo mostrar menu

--- paquetes/menus/menu_ordenes.py
+++ paquetes/menus/menu_ordenes.py
@@ -2,6 +2,7 @@ from ..listas.pizzas import Pizzas
 from ..listas.ordenes import Ordenes
 from ..componentes.consola import Consola
 from ..listas.ingredientes import Ingredientes
+from ..enums.o_menu_ordenes import OMenuOrdenes
 
 class MenuOrdenes:
     __pizzas: Pizzas = None
@@ -18,5 +19,28 @@ class MenuOrdenes:
         
         while volver == False:
             Consola.limpiar_consola()
-            print('-'*20 + 'MENU ORDENES'  + + '-'*20)
-                        
+            print('-'*20 + 'MENU ORDENES'  + '-'*20)
+            for orden in self.__ordenes:
+                print('{}. {}'.format(orden.get_numero(), orden.get_cliente().get_nombre()))
+            
+            print('{}. {}'.format(OMenuOrdenes.Orden_Completa.value, OMenuOrdenes.Orden_Completa.name.replace('_', ' ')))
+            print('{}. {}'.format(OMenuOrdenes.CrearOrden.value, OMenuOrdenes.CrearOrden.name.replace('_', ' ')))
+            print('{}. {}'.format(OMenuOrdenes.Volver.value, OMenuOrdenes.Volver.name.replace('_', ' ')))
+            
+            print('Ingrese el número de la opción que desea seleccionar:')
+            opcion = input()
+            
+            if opcion.isnumeric():
+                if int(opcion) == int(OMenuOrdenes.Orden_Completa.value):
+                    self.__ordenes.desencolar()
+                if int(opcion) == int(OMenuOrdenes.CrearOrden.value):
+                    self.__crear_orden()
+                elif int(opcion) == int(OMenuOrdenes.Volver.value):
+                    volver = True
+    
+    def __crear_orden(self):
+        salir = False
+        
+        while salir == False:
+            Consola.limpiar_consola()
+            

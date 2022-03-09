9929431d2e0268f19ac137f0a42414aef322ffc3
Axel Esquivel <2073218250101@ingenieria.usac.edu.gt>
2022-03-09 03:01:00 -0600

    agregados parametros menu ordenes

--- paquetes/menus/menu_ordenes.py
+++ paquetes/menus/menu_ordenes.py
@@ -1,10 +1,16 @@
+from ..listas.pizzas import Pizzas
 from ..listas.ordenes import Ordenes
+from ..listas.ingredientes import Ingredientes
 
 class MenuOrdenes:
+    __pizzas: Pizzas = None
     __ordenes: Ordenes = None
+    __ingredientes: Ingredientes = None
     
-    def __init__(self, ordenes: Ordenes):
+    def __init__(self, ordenes: Ordenes, pizzas: Pizzas, ingredientes: Ingredientes):
+        self.__pizzas = pizzas
         self.__ordenes = ordenes
+        self.__ingredientes = ingredientes
     
     def mostrar_menu(self):
         volver = False

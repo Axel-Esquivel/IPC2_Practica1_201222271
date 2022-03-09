d5ed7cfd8608c7667fadf0276aaedd06c19b4697
Axel Esquivel <2073218250101@ingenieria.usac.edu.gt>
2022-03-08 19:10:16 -0600

    corrección en recorrido de indices

--- paquetes/listas/ordenes.py
+++ paquetes/listas/ordenes.py
@@ -37,11 +37,11 @@ class Ordenes:
     def eliminar_indice(self, indice: int) -> Orden:
         self.__temporal = self.__puntero
         
-        indice_recorrido = -1
+        indice_recorrido = 0
         while indice_recorrido != indice:
-            indice_recorrido += 1
             if self.__temporal.get_siguiente() != None:
                 self.__temporal = self.__temporal.get_siguiente()
+            indice_recorrido += 1
         
         if indice_recorrido == indice and self.__temporal.get_siguiente() != None:
             self.__conteo -= 1

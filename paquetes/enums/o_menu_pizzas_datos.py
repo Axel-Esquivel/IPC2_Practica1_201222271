12422b58fcfc627f63d76435c6cf4f815e200c93
Axel Esquivel <2073218250101@ingenieria.usac.edu.gt>
2022-03-09 01:58:09 -0600

    creado opciones menu pizza datos

---
+++ paquetes/enums/o_menu_pizzas_datos.py
@@ -0,0 +1,8 @@
+from enum import Enum, auto
+
+class OMenuPizzasDatos(Enum):
+    Nombre = auto()
+    Precio = auto()
+    Agregar_Ingrediente = auto()
+    Eliminar_Ingrediente = auto()
+    Cancelar = auto()

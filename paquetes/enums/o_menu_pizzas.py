1f3ed3a2c93c4cdc833f25a2b50b772b7452a50b
Axel Esquivel <2073218250101@ingenieria.usac.edu.gt>
2022-03-08 19:55:25 -0600

    mod orden enums

--- paquetes/enums/o_menu_pizzas.py
+++ paquetes/enums/o_menu_pizzas.py
@@ -3,7 +3,7 @@ from enum import Enum, auto
 
 class OMenuPizzas(Enum):
     Crear_Pizza = auto()
+    Eliminar_Pizza = auto()
     Agregar_Ingredientes = auto()
     Eliminar_Ingredientes = auto()
-    Eliminar_Pizza = auto()
     Volver = auto()

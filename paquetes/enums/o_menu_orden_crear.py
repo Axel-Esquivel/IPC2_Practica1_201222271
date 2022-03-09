a53fef428af551b6763eaf62ebe7f79d3acaf0a7
Axel Esquivel <2073218250101@ingenieria.usac.edu.gt>
2022-03-09 12:00:38 -0600

    creada clase o menu orden crear

---
+++ paquetes/enums/o_menu_orden_crear.py
@@ -0,0 +1,11 @@
+from enum import Enum, auto
+
+class OMenuOrdenCrear(Enum):
+    Nombre = auto()
+    Direccion = auto()
+    NIT = auto()
+    Agregar_Pizzas = auto()
+    Eliminar_Pizzas = auto()
+    Guardar = auto()
+    Cancelar = auto()
+    
